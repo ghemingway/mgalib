35261fbc9f19abb9166b9a75aff8eefef551ff19
graham.hemingway <graham.hemingway@8932de9b-a0df-7518-fb39-9aee4a96b462>
2010-12-23 21:39:44 +0000

    Start of MetaProject testing.  Long ways to go.  New MetaProjectTest.CreateMetaGMEv3 fails really well.

---
+++ trunk/Testing/MetaProjectTesting.h
@@ -0,0 +1,13 @@
+#ifndef __META_PROJECT_TESTING_H__
+#define __META_PROJECT_TESTING_H__
+
+
+/*** Included Header Files ***/
+#include "../Meta/MetaProject.h"
+#include <gtest/gtest.h>
+
+
+#endif // __META_PROJECT_TESTING_H__
+
+
+
